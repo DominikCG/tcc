3cda84f3f8c1427879c2fcf619ac060c6d3e6a5e
DominikCG <dominikc.gause@gmail.com>
2023-06-19 14:20:25 -0300

    ajustes

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    private bool isOverlaping = false;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isOverlaping){
+                player.GetComponent<PlayerMovement>().Warp(new Vector2(player.transform.position.x,this.transform.position.y+1.5f));
+                player.GetComponent<PlayerHealth>().TakeDamage(1);
+        }
     }
 
     private void FixedUpdate() {
@@ -22,8 +27,9 @@ public class Block : MonoBehaviour
         if(player != null){
             BoxCollider2D playerCollider = player.GetComponent<BoxCollider2D>();
             if(this.GetComponent<BoxCollider2D>().bounds.Intersects(playerCollider.bounds)){
-                player.GetComponent<PlayerHealth>().TakeDamage(1);
-                player.GetComponent<PlayerMovement>().Warp(new Vector2(player.transform.position.x,this.transform.position.y+1));
+                isOverlaping = true;
+            }else{
+                isOverlaping = false;
             }
 
         }
